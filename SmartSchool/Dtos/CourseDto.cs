6551760c6c24b97efb6f75e1f4109a8704c7165e
João Denilson S. dos Santos <denilson_santos@virgo.atlantico.net.br>
2022-02-04 09:56:53 -0300

    Update Dtoś

---
+++ SmartSchool/Dtos/CourseDto.cs
@@ -0,0 +1,15 @@
+﻿using SmartSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Dtos
+{
+    public class CourseDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public IEnumerable<Discipline> Disciplines { get; set; }
+    }
+}
---
+++ SmartSchool/Dtos/TeacherDto.cs
@@ -0,0 +1,18 @@
+﻿using SmartSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartSchool.Dtos
+{
+    public class TeacherDto
+    {
+        public int Id { get; set; }
+        public int RegistrationId { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public bool Active { get; set; } = true;
+        public IEnumerable<Discipline> Disciplines { get; set; }
+    }
+}
