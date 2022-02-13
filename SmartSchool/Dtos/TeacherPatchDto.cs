faf84afc48754fa4838f5e72d61c4399dbbe4c1b
João Denilson S. dos Santos <denilson_santos@virgo.atlantico.net.br>
2022-02-13 15:28:45 -0300

    Update Patch

--- SmartSchool/Dtos/TeacherPatchDto.cs
+++ SmartSchool/Dtos/TeacherPatchDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SmartSchool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,7 +15,7 @@ namespace SmartSchool.Dtos
 #pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         public string? Phone { get; set; }
-#pragma warning restore CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
         public bool? Active { get; set; } = true;
+        public IEnumerable<Discipline> Disciplines { get; set; }
     }
 }
